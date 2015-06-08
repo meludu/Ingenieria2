4968d602924611f0ce869697cb2fd55904c0b6c6
Iván Salgado <ivansalgado2243@gmail.com>
2015-06-08 02:19:03 -0300

    Editar categoria
    Ahora se muestra la categoria en el input.

--- ingenieria/content/admin/editarCateg.php
+++ ingenieria/content/admin/editarCateg.php
@@ -23,7 +23,7 @@ $res_query = mysqli_fetch_array($query);
             <form name="modif_categ_frm" action="procesarEditarCateg.php?idCategoria=<?php echo $id_categ?>" method="post">
               <div class="form-group">
                 <label for="categoriaEdit">Modificar la categor&iacute;a <i><?php echo "<b>".utf8_encode($res_query["nombre_cat"])."</b>".":";?></i></label>
-                <input type="text" class="form-control" id="categoriaEdit" name="editarCategoriaTxt" placeholder="Ingrese nombre de categor&iacute;a">
+                <input type="text" class="form-control" id="categoriaEdit" name="editarCategoriaTxt" value="<?php echo $res_query["nombre_cat"]; ?>" placeholder="Ingrese nombre de categor&iacute;a">
               </div>
               <div class="form-group">
                 <input type="button" class="btn btn-default" value="Modificar" onclick="validarModificarCateg()">
