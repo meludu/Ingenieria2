3cb9436f61e0fc6b827ea5aaddedc7a19cdb581a
Iván Salgado <ivansalgado2243@gmail.com>
2015-06-08 23:01:56 -0300

    Ahora se puede ver cada publicación.

---
+++ ingenieria/content/prod.php
@@ -0,0 +1,75 @@
+<?php
+    $query = "SELECT * FROM categorias ORDER BY nombre_cat";   // Consulta de las categorias
+    $res = mysqli_query($link,$query);
+?>
+<div class="container paddingWithoutNav">
+        <div class="row">
+            <div class="col-md-4">
+                <p class="lead">Bestnid - Subastas</p>
+                <!--<div class="list-group"> -->
+                <ul id="side">
+                    <?php
+                    while ($tupla = mysqli_fetch_array($res)) { ?>
+                            <li><a href="?op=prod&idC=<?php echo $tupla['idCategoria']; ?>"><?php echo $tupla["nombre_cat"]; ?></a></li>
+                    <?php  
+                        } 
+                    ?>
+                </ul>
+                <!--</div> -->
+            </div>
+	<div class="col-md-8">              
+                <div style="float:right">
+                    <i class="fa fa-calendar"></i> <a href="?op=prod&idC=<?php echo $_GET['idC']; ?>&orden1=<?php echo "DESC"; ?>" title="Lo que no esta por terminar"><i class="fa fa-arrow-up"></i></a> <a href="?op=prod&idC=<?php echo $_GET['idC']; ?>&orden2=<?php echo "ASC"; ?>" title="Lo que esta por terminar"><i class="fa fa-arrow-down"></i></a>
+                    <i class="fa fa-eye"></i> <a href="?op=prod&idC=<?php echo $_GET['idC']; ?>&orden3=<?php echo "DESC"; ?>" title="Lo mas vistos"><i class="fa fa-arrow-up"></i></a> <a href="?op=prod&idC=<?php echo $_GET['idC']; ?>&orden4=<?php echo "ASC"; ?>" title="Lo menos visto"><i class="fa fa-arrow-down"></i></a>
+                </div>
+                <div style="clear:both"></div>   <!-- Uso esto para que no se me junte con el DIV de abajo -->
+                <div class="row">
+                    <?php 
+                        $orden = "idProducto";
+                        $tipoOrden = "ASC";
+                        if (isset($_GET['orden1'])) {
+                            $orden = "fecha_fin";
+                            $tipoOrden = "DESC";
+                        }
+                        if (isset($_GET['orden2'])) {
+                            $orden = "fecha_fin";
+                            $tipoOrden = "ASC";
+                        }
+                        if (isset($_GET['orden3'])) {
+                            $orden = "visitas";
+                            $tipoOrden = "DESC";
+                        }
+                        if (isset($_GET['orden4'])) {
+                            $orden = "visitas";
+                            $tipoOrden = "ASC";
+                        }
+                        $idCat = $_GET['idC'];
+                        $queryPro = "SELECT idProducto, nombre, descripcionCorta, visitas, fecha_fin FROM productos WHERE idCategoria = $idCat ORDER BY $orden $tipoOrden";
+                        $resPro = mysqli_query($link,$queryPro);
+                        $resContarPro = mysqli_query($link,$queryPro);
+                        $cantidadDePro = mysqli_num_rows($resContarPro);
+                        if ($cantidadDePro > 0) {
+                        while ($tuplaPro = mysqli_fetch_array($resPro)) {
+                    ?>
+                    <!-- Inicia el primer producto -->
+                    <div class="col-sm-4 col-lg-4 col-md-4">
+                        <div class="thumbnail">
+                            <img style="max-width: 320px; max-height: 150px;" src="content/imagen_portada.php?idPro=<?php echo $tuplaPro['idProducto']; ?>" >
+                            <div class="caption">
+                                <h4 class="pull-right"></h4>
+                                <h4><a href="?op=publicacion&idP=<?php echo $tuplaPro['idProducto']; ?>"><?php echo $tuplaPro['nombre']; ?></a></h4>
+                                <p><?php echo $tuplaPro['descripcionCorta']; ?></p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right"><?php echo $tuplaPro['visitas']." "; ?><i class="fa fa-eye"></i></p>
+                                <p><?php echo $tuplaPro['fecha_fin']." "; ?><i class="fa fa-calendar"></i></p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Finaliza el primer producto -->
+                    <?php } 
+                    }else{ ?>
+                        <h3><p>Todavia no existen productos que se subasten para esta categoria. </p></h3>
+                    <?php } ?>
+                </div>
+    </div>
---
+++ ingenieria/content/publicacion.php
@@ -0,0 +1,124 @@
+    <?php
+        // Incremento la cantidad de vistos.
+        $queryVistos = "UPDATE productos SET visitas = visitas + 1 WHERE idProducto = '".$_GET['idP']."' ";
+        mysqli_query($link,$queryVistos);
+        // Selecciono el producto.
+        $queryPro = "SELECT * FROM productos WHERE idProducto = '".$_GET['idP']."' ";
+        $resPro = mysqli_query($link,$queryPro);
+        $tuplaPro = mysqli_fetch_array($resPro);
+    ?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-3">
+                <p class="lead">Shop Name</p>
+                <div class="list-group">
+                    <a href="#" class="list-group-item active">Category 1</a>
+                    <a href="#" class="list-group-item">Category 2</a>
+                    <a href="#" class="list-group-item">Category 3</a>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="http://placehold.it/800x300" alt="">
+                    <div class="caption-full">
+                        <h4 class="pull-right"><i>Se publico el: <?php echo $tuplaPro['fecha_ini']; ?></i></h4>
+                        <h4><a href="#"><?php echo $tuplaPro['nombre']; ?></a>
+                        </h4>
+                        <p><?php echo $tuplaPro['descripcionCorta']; ?></p>
+                        <br/>
+                        <p><?php echo $tuplaPro['descripcionLarga']; ?></p>
+                    </div>
+                    <?php 
+                        $consultaFecha = "SELECT CURDATE()"; 
+                        $resFecha = mysqli_query($link,$consultaFecha); 
+                        $fechaActual = mysqli_fetch_array($resFecha); 
+                        include("connect/calcular_fecha.php");
+                    ?>                  
+                    <div class="ratings">
+                        <p class="pull-right"><?php echo $tuplaPro['visitas']." "; ?><i class="fa fa-eye"></i></p>
+                        <p><?php echo interval_date($fechaActual[0],$tuplaPro['fecha_fin']); ?></p>
+                    </div>
+                </div>
+
+                <div class="well">
+                    <?php
+                        $consCantPre = "SELECT COUNT(*) FROM productos p INNER JOIN preguntas_productos pp ON(p.idProducto = pp.idProducto) INNER JOIN preguntas pre ON(pp.idPregunta = pre.idPregunta) WHERE p.idProducto = '".$_GET['idP']."' ";
+                        $resCantPre = mysqli_query($link,$consCantPre);
+                        $cantidadPre = mysqli_fetch_array($resCantPre);
+                    ?>
+                    <div class="text-right">
+                        <p>Preguntas totales: <?php echo $cantidadPre[0]; ?></p>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star-empty"></span>
+                            Anonymous
+                            <span class="pull-right">10 days ago</span>
+                            <p>This product was great in terms of quality. I would definitely buy another!</p>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star-empty"></span>
+                            Anonymous
+                            <span class="pull-right">12 days ago</span>
+                            <p>I've alredy ordered another one!</p>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star"></span>
+                            <span class="glyphicon glyphicon-star-empty"></span>
+                            Anonymous
+                            <span class="pull-right">15 days ago</span>
+                            <p>I've seen some better than this, but not at this price. I definitely recommend this item.</p>
+                        </div>
+                    </div>
+                    <?php
+                        if (isset($_SESSION['estado']) && $_SESSION['estado'] == "online") {
+                    ?>
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p>Escribe un comentario: </p>
+                            <form action="" method="POST">
+                                <textarea class="form-control" style="resize:none;" name="comentario" rows="3" cols="112">
+                                </textarea><br/>
+                                <input class="btn btn-primary" style="float:right;" type="submit" name="boton" value="Comentar">                                 
+                            </form>
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
